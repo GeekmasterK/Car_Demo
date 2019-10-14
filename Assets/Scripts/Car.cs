8b7e78af535ff9ef69a2eabc2f16fe48cbf68e29
GeekmasterK <ktheiss1990@gmail.com>
2019-10-14 16:03:28 -0700

    Added deceleration and braking.

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -4,8 +4,13 @@ public class Car : MonoBehaviour
 {
     [SerializeField] float maxSpeed = 6f; // maximum speed
     [SerializeField] float accelTime = 2.5f; // time to reach 0 to max speed
+    [SerializeField] float decelTime = 6f; // time to reach max speed to 0
+    [SerializeField] float brakeTime = 1f; // time to reach max speed to 0 when braking
+
     float accelRatePerSec; // rate of acceleration per second
     float forwardVelocity; // velocity of the car on the z-axis
+    float decelRatePerSec; // rate of deceleration per second
+    float brakeRatePerSec; // rate of deceleration per second when braking 
 
     Rigidbody rigidBody;
 
@@ -14,35 +19,41 @@ public class Car : MonoBehaviour
     {
         rigidBody = GetComponent<Rigidbody>();
         accelRatePerSec = maxSpeed / accelTime;
+        decelRatePerSec = -maxSpeed / decelTime;
+        brakeRatePerSec = -maxSpeed / brakeTime;
         forwardVelocity = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        RespondToAccelInput();
-        // TODO add braking/deceleration
+        RespondToAccelInput();        
         // TODO add left/right steering
     }
 
     /*
      * accelerate when pressing the spacebar (applying the gas)
      */
-    private void RespondToAccelInput()
+    void RespondToAccelInput()
     {
         if (Input.GetKey(KeyCode.W)) // can accelerate while turning
         {
-            ApplyAcceleration();
+            Accelerate(accelRatePerSec);
+        }
+        else if(Input.GetKey(KeyCode.Z))
+        {
+            Accelerate(brakeRatePerSec);
+        }
+        else
+        {
+            Accelerate(decelRatePerSec);
         }
     }
 
-    /*
-     * move the car forward by acceleration
-     */
-    private void ApplyAcceleration()
+    void Accelerate(float accel)
     {
-        forwardVelocity += accelRatePerSec * Time.deltaTime; // increase forward velocity by accelRatePerSec once per frame
-        forwardVelocity = Mathf.Min(forwardVelocity, maxSpeed); // prevent forward velocity from exceeding maximum speed
+        forwardVelocity += accel * Time.deltaTime; // increase forward velocity by accelRatePerSec once per frame
+        forwardVelocity = Mathf.Clamp(forwardVelocity, 0f, maxSpeed); // prevent forward velocity from exceeding maximum speed
         rigidBody.velocity = Vector3.forward * forwardVelocity; // accelerate forward
     }
 }
