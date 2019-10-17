52cd30e188bd58d1a945571cfdd9135541f48834
GeekmasterK <ktheiss1990@gmail.com>
2019-10-17 11:04:09 -0700

    Object in scene added for testing. Steering controls implemented.

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -2,15 +2,24 @@
 
 public class Car : MonoBehaviour
 {
+    // car attributes
     [SerializeField] float maxSpeed = 6f; // maximum speed
     [SerializeField] float accelTime = 2.5f; // time to reach 0 to max speed
     [SerializeField] float decelTime = 6f; // time to reach max speed to 0
     [SerializeField] float brakeTime = 1f; // time to reach max speed to 0 when braking
+    [SerializeField] float turnAnglePerSec = 90f; // steering angle adjustment per second
+    float leftTurn = -1f; // modifier to calculate left turn
+    float rightTurn = 1f; // modifier to calculate right turn
 
+    // attributes calculated at runtime
     float accelRatePerSec; // rate of acceleration per second
-    float forwardVelocity; // velocity of the car on the z-axis
     float decelRatePerSec; // rate of deceleration per second
     float brakeRatePerSec; // rate of deceleration per second when braking 
+
+    // current car state
+    float forwardVelocity; // velocity of the car on the z-axis
+    float currentRotation; // current car rotation
+    bool accelChange;
 
     Rigidbody rigidBody;
 
@@ -27,8 +36,8 @@ public class Car : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RespondToAccelInput();        
-        // TODO add left/right steering
+        RespondToAccelInput();
+        RespondToTurnInput();
     }
 
     /*
@@ -40,7 +49,7 @@ public class Car : MonoBehaviour
         {
             Accelerate(accelRatePerSec);
         }
-        else if(Input.GetKey(KeyCode.Z))
+        else if (Input.GetKey(KeyCode.Z))
         {
             Accelerate(brakeRatePerSec);
         }
@@ -50,10 +59,44 @@ public class Car : MonoBehaviour
         }
     }
 
+    void RespondToTurnInput()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Turn(leftTurn);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Turn(rightTurn);
+        }
+    }
+
     void Accelerate(float accel)
     {
         forwardVelocity += accel * Time.deltaTime; // increase forward velocity by accelRatePerSec once per frame
         forwardVelocity = Mathf.Clamp(forwardVelocity, 0f, maxSpeed); // prevent forward velocity from exceeding maximum speed
-        rigidBody.velocity = Vector3.forward * forwardVelocity; // accelerate forward
+        rigidBody.velocity = transform.forward * forwardVelocity; // accelerate forward
+        accelChange = true;
+    }
+
+    void Turn(float direction)
+    {
+        currentRotation = turnAnglePerSec * Time.deltaTime * direction;
+        
+        // if moving forward, turn car
+        if (forwardVelocity > 0)
+        {
+            rigidBody.rotation = Quaternion.Euler(rigidBody.rotation.eulerAngles + new Vector3(0f, currentRotation, 0f));
+        }
+        
+        // if no acceleration input, decelerate
+        if(!accelChange)
+        {
+            Accelerate(decelRatePerSec);
+        }
+
+        // reset for next frame
+        accelChange = false;
+        currentRotation = 0f;
     }
 }
