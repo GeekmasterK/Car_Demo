27085316891a50b91d6bb08f32a1cec86c95b2db
GeekmasterK <ktheiss1990@gmail.com>
2019-10-18 07:36:07 -0700

    Implemented a function for the camera to rotate behind the player when they change the direction of the car.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,8 +5,23 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] Transform target; // target object for camera to follow
     [SerializeField] float smoothSpeed = 10f; // speed of camera movement
     [SerializeField] Vector3 offset; // camera position in relation to the target
-
+    
     void FixedUpdate()
+    {
+        CameraMove();
+        CameraRotate();
+    }
+
+    void CameraRotate()
+    {
+        Vector3 desiredRotation = target.position + (-target.forward * offset.magnitude);
+        Vector3 smoothedRotation = Vector3.Lerp(transform.position, desiredRotation, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedRotation;
+
+        transform.LookAt(target);
+    }
+
+    void CameraMove()
     {
         Vector3 desiredPosition = target.position + offset; // assign the desired position of the camera in relation to the player
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime); // interpolate the camera's movement vector
