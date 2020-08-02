a008f3ade5f80299ad3fc94b05964e5d09b21c73
GeekmasterK <ktheiss1990@gmail.com>
2020-08-02 09:38:07 -0700

    Revert "Revert "Clean up and comment code for readability.""
    This reverts commit 5d59be488a969c015e560484891aa4bf19cba677.

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public float accelerate; // acceleration input variable
+    public float steer; // steering input variable
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetInputValues();
+    }
+
+    /*
+     * assign values to the accelerate and steer input variables
+     */
+    void SetInputValues()
+    {
+        accelerate = Input.GetAxisRaw("Vertical"); // accelerate forward or backward
+        steer = Input.GetAxisRaw("Horizontal"); // steer left or right
+    }
+}
