3e245a21c42d4709a39120edd9c3bd03f8715bfb
GeekmasterK <ktheiss1990@gmail.com>
2020-08-02 09:37:55 -0700

    Revert "Merge branch 'master' of https://github.com/GeekmasterK/Car_Demo into HEAD"
    This reverts commit 79e1840c008fad3bc3db76aba9e4d3f897c89f54.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -12,20 +12,26 @@ public class CameraFollow : MonoBehaviour
         CameraRotate();
     }
 
-    void CameraRotate()
-    {
-        Vector3 desiredRotation = target.position + (-target.forward * offset.magnitude);
-        Vector3 smoothedRotation = Vector3.Lerp(transform.position, desiredRotation, smoothSpeed * Time.deltaTime);
-        transform.position = smoothedRotation;
-
-        transform.LookAt(target);
-    }
-
+    /*
+     * move the camera behind the player
+     */
     void CameraMove()
     {
         Vector3 desiredPosition = target.position + offset; // assign the desired position of the camera in relation to the player
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime); // interpolate the camera's movement vector
         transform.position = smoothedPosition; // update the camera's position to follow the movement vector
+
+        transform.LookAt(target); // make sure the target is always in view of the camera
+    }
+
+    /*
+     * rotate the camera to face the player
+     */
+    void CameraRotate()
+    {
+        Vector3 desiredRotation = target.position + (-target.forward * offset.magnitude); // assign the desired rotation of the camera in relation to the player
+        Vector3 smoothedRotation = Vector3.Lerp(transform.position, desiredRotation, smoothSpeed * Time.deltaTime); // interpolate the camera's rotation vector
+        transform.position = smoothedRotation; // update the camera's rotation to follow the rotation vector
 
         transform.LookAt(target); // make sure the target is always in view of the camera
     }
